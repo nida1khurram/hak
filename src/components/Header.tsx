c20cf6eba523c0b9fdb3dad511ab97da27e30930
nida1khurram <147607279+nida1khurram@users.noreply.github.com>
2024-12-08 12:46:48 +0500

    hero & header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,10 +42,10 @@ export default function Header() {
   return (
     <header className="w-full md:max-w-[1929px] mx-auto bg-black font-inter text-base leading-6 text-white py-4 px-6">
       <Link href="#" className='flex justify-center'><Image src="/logo.png" alt="logo" width={109} height={32}/></Link>
-      <div className="px-80 flex justify-between items-center">
+      <div className="md:px-80 flex flex-col md:flex-row justify-between items-center">
         {/* <Link href="/" className="text-2xl font-bold text-[#ba9b54]">Food<span>luck</span></Link> */}
         <nav>
-          <ul className="flex space-x-8">
+          <ul className="flex flex-col md:flex-row md:space-x-8 mt-4 md:mt-0">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/">Menu</Link></li>
             <li><Link href="/">Blog</Link></li>
@@ -55,11 +55,11 @@ export default function Header() {
             <li><Link href="/">Contact</Link></li>
           </ul>
         </nav>
-        <div className="relative ml-40">
+        <div className="relative md:ml-40 mt-4 md:mt-0">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent border-2 border-[#ff9F0D] text-white rounded-full py-2 px-4 pr-10 w-[310px] h-[54px] focus:outline-none "
+            className="bg-transparent border-2 border-[#ff9F0D] text-white rounded-full py-2 px-4 pr-0 md:pr-10 w-full h-auto md:w-[310px] md:h-[54px] focus:outline-none "
           />
           <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 " />
           <MdOutlineShoppingBag className='w-6 h-6 absolute right-[-30px] top-1/2 transform -translate-y-1/2 text-gray-400'/>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,30 +20,31 @@ export default function Hero() {
           {/* left side */}
       <div className='w-[45%]'> 
      
-      <div className="relative z-20 h-full flex flex-col justify-center items-start px-10">
-        <div className='md:w-[27px] md:h-[492px] absolute mt-96 ml-[-20px]'>
+      <div className="relative z-20 h-full flex flex-col justify-center items-start px-10 mt-10 md:mt-0">
+        <div className='md:w-[27px] md:h-[492px] absolute mt-[-100px] md:mt-96 ml-[-20px]'>
         <FaFacebookF className='text-[#ff9F0D] mb-5'/>
         <FaTwitter className='text-[#ff9F0D] mb-5'/>
         <SlSocialPintarest className='text-[#ff9F0D] mb-5'/>
         </div>
         <div className='ml-5'>
         <h2 className="text-[#ff9F0D] font-greatvibes text-2xl font-normal mb-2 leading-10">It`&apos;s Quick & Amazing !</h2>
-        <h1 className="text-6xl font-bold text-[#ff9F0D] leading-[68px] mb-8">Th<span className='text-white'>e Art of speed<br />food Quality</span></h1>
+        <h1 className="text-3xl md:text-6xl font-bold text-[#ff9F0D] leading-10 md:leading-[68px] mb-2 md:mb-8 ">Th<span className='text-white'>e Art of speed food Quality</span></h1>
         <p className="text-base text-white font-normal leading-6 mb-8 max-w-md">Lorem ipsum dolor sit amet, consectetur adipiscingelit. Nullam at enim mauris.</p>
-        <Link href="/" className="bg-[#ff9F0D] text-white font-inter text-base px-14 py-4 rounded-full hover:bg-orange-600 transition duration-300">
+      
+      </div>
+      <Link href="/" className="bg-[#ff9F0D] text-white font-inter text-base px-10 md:px-14 py-1 md:py-4 rounded-full hover:bg-orange-600 transition duration-300 md:ml-0 ml-32 md:mt-0 mt-[-80px]">
           See Menu
         </Link>
       </div>
       </div>
-      </div>
         {/* right side */}
-        <div className='w-[55%]'> 
+        <div className='w-[55%] '> 
           <Image
           src="/hero/hero.png"
           alt="Hero dish"
           width={877.7}
           height={670}
-          className="z-0"
+          className="z-0 md:mt-0 mt-3"
         /></div>
      
       </div>
